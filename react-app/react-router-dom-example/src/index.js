e3c79cdfd2919dc09d23ef7d20ae26bcdae452ae
wootaepark <pakdal99@gmail.com>
2024-04-01 13:56:16 +0900

    react-router-dom-2nd-commit

--- react-app/react-router-dom-example/src/index.js
+++ react-app/react-router-dom-example/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
 
 function Home(){
   return(
@@ -31,16 +32,25 @@ function App(){
   return(
     <div>
       <h1>Hello React Router DOM</h1>
+      <ul>
+        <li><a href='/'>Home</a></li>
+        <li><a href='/topics'>Topics</a></li>
+        <li><a href='/contact'>Contact</a></li>
+      </ul>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/topics' element={<Topics></Topics>}></Route>
+        <Route path='/contact' element={<Contact></Contact>}></Route>
+        <Route path='/*' element={<h2>Not Found</h2>}></Route>
+      </Routes>
 
-      <Home></Home>
-      <Topics></Topics>
-      <Contact></Contact>
+      
 
     </div>
   )
 }
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')).render(<BrowserRouter><App /></BrowserRouter>);
 // index.html에 'root'라는 아이디를 가진 요소가 있다.
 
 // If you want to start measuring performance in your app, pass a function
